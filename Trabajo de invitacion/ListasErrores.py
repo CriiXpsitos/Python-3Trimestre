49e264b2f6c39beaac3dbf0e506854f970ef1118
CriiXpsitos <cristianpee2609@gmail.com>
2023-10-26 08:42:06 -0500

    codigo errores

--- Trabajo de invitacion/ListasErrores.py
+++ Trabajo de invitacion/ListasErrores.py
@@ -1,6 +1,5 @@
-import time
 aprendices_edades = [['Sebastian Tovar', 'Juan Mahecha', 'Maria Buenaventura', 'Mathew Guarnizo', 'Kevin Hernan', 'Melissa Lopez', 'Vanesa Ladino', 'Stiven Ramirez', 'Esteban Prada', 'Kevin Botero', 'Sebastian Pinzon', 'Camila Alape', 'Kevin Londoño', 'Nicolas Fierro', 'Santiago Gomez', 'Marlon Lopez ', 'Stiwar Perez', 'Miguel Bernal', 'Maria Molano', 'Nicolas Paulo', 'Laura Benavides', 'Wilfrank', 'Dahiana', 'Nataly', 'Maria José', 'Cristian Peña', 'Paula', 'Kevin Eduardo', 'Yency', 'Matias Guzman'],
-                      [17, 17, 17, 17, 17, 17, 17, 18, 17, 17, 17, 18, 23, 18, 18, 19, 18, 17, 18, 17, 17, 18, 18, 18, 18, 18, 18, 18, 18, 18]]
+                      int[17, 17, 17, 17, 17, 17, 17, 18, 17, 17, 17, 18, 23, 18, 18, 19, 18, 17, 18, 17, 17, 18, 18, 18, 18, 18, 18, 18, 18, 18]]
 
 
 
@@ -18,37 +17,39 @@ for nombre, edad in zip(aprendices_edades[0], aprendices_edades[1]):
 
 #poner el nombre de la instructora en el primero
 
-aprendices_edades[0].insert(0, "Adriana lucia rincon Forero" )
+#error 1
+aprendices_edades[1].insert(0, "Adriana lucia rincon Forero" )
     
 print("----------------INSTRUCTORA---------------------------")
 print(aprendices_edades [0])
 
-#aprendices_edad[1] max age
-
+#error 2
 print("---------- la persona con mayor edad ---------------")
 
-edadMax=max(aprendices_edades[1])
-print("---------- la edad maxima es ------------")
-print(edadMax)
+# edadMax=max(aprendices_edades[1])
+# print("---------- la edad maxima es ------------")
+# print(edadMax)
+
 
 # # Encontrar la edad máxima en la lista de edades
-# edadMax = max(aprendices_edades[1])
+edadMax = max(aprendices_edades[1])
 
-# # Encontrar el índice de la edad máxima en la lista de edades
-# indiceMax = aprendices_edades[1].index(edadMax)
+# Encontrar el índice de la edad máxima en la lista de edades
+indiceMax = aprendices_edades[1].index(edadMax)
 
-# # Obtener el nombre de la persona con la edad máxima
-# nombreMax = aprendices_edades[0][indiceMax]
+# Obtener el nombre de la persona con la edad máxima
+nombreMax = aprendices_edades[0][indiceMax]
 
-# # Imprimir la persona con la edad máxima
-# print(f"La persona con la mayor edad es: {nombreMax}, tiene {edadMax} años")
+# Imprimir la persona con la edad máxima
+print(f"La persona con la mayor edad es: {nombreMax}, tiene {edadMax} años")
 
 
 
 # cuantos aprendices tienen 18 
 
+#error 3
 print("---------- cuantas personas con 18 hay?-----------")
-conteoEdad18 = print(aprendices_edades[1].count(18))
+conteoEdad18 = print(aprendices_edades[0].count("18"))
 
 #agregar una persona random al final
 print("--------- estudiante al final de la lista---------------------")
