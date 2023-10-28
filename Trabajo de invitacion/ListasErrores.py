75dcb0051b3c4384a776ec2610f89571bf3dcfb4
CriiXpsitos <cristianpee2609@gmail.com>
2023-10-28 13:43:25 -0500

    Update ListasErrores.py

--- Trabajo de invitacion/ListasErrores.py
+++ Trabajo de invitacion/ListasErrores.py
@@ -1,5 +1,5 @@
 aprendices_edades = [['Sebastian Tovar', 'Juan Mahecha', 'Maria Buenaventura', 'Mathew Guarnizo', 'Kevin Hernan', 'Melissa Lopez', 'Vanesa Ladino', 'Stiven Ramirez', 'Esteban Prada', 'Kevin Botero', 'Sebastian Pinzon', 'Camila Alape', 'Kevin Londoño', 'Nicolas Fierro', 'Santiago Gomez', 'Marlon Lopez ', 'Stiwar Perez', 'Miguel Bernal', 'Maria Molano', 'Nicolas Paulo', 'Laura Benavides', 'Wilfrank', 'Dahiana', 'Nataly', 'Maria José', 'Cristian Peña', 'Paula', 'Kevin Eduardo', 'Yency', 'Matias Guzman'],
-                      int[17, 17, 17, 17, 17, 17, 17, 18, 17, 17, 17, 18, 23, 18, 18, 19, 18, 17, 18, 17, 17, 18, 18, 18, 18, 18, 18, 18, 18, 18]]
+                      [17, 17, 17, 17, 17, 17, 17, 18, 17, 17, 17, 18, 23, 18, 18, 19, 18, 17, 18, 17, 17, 18, 18, 18, 18, 18, 18, 18, 18, 18]]
 
 
 
